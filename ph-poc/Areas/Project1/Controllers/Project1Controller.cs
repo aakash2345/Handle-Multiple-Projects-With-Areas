5dfc2de4c85df8c587ab5fb0558e8f807e9a473c
Aakash Nethi <aakash.nethi@parker.com>
2018-08-30 08:48:49 -0400

    Add project files.

---
+++ ph-poc/Areas/Project1/Controllers/Project1Controller.cs
@@ -0,0 +1,16 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Threading.Tasks;
+//using Microsoft.AspNetCore.Http;
+//using Microsoft.AspNetCore.Mvc;
+
+//namespace ph_poc.Areas.Project1.Controllers
+//{
+//    [Area("Project1")]
+//    public class Project1Controller : Controller
+//    {
+//        // GET: Home
+
+//    }
+//}
